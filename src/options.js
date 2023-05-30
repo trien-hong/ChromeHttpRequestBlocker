c28d54a177ea92d2734db2f58dd57afcef77fec2
trien-hong <danielhong55@gmail.com>
2023-05-30 00:34:36 -0400

    changed variables & functions name and removed unneeded variables

--- src/options.js
+++ src/options.js
@@ -10,8 +10,6 @@ app.controller('OptionsController', function($scope) {
         };
     });
 
-    $scope.function = "";
-
     $scope.button_is_pause_color = $scope.backgroundPage.button_is_pause_color;
 
     $scope.is_pause = $scope.backgroundPage.is_pause;
@@ -33,7 +31,7 @@ app.controller('OptionsController', function($scope) {
                 $scope.is_pause = "Unpause extension";
                 $scope.button_is_pause_color = "btn-success";
                 
-                $scope.alert("Extension is now PAUSED. All patterns will not be blocked.");
+                $scope.alertModal("Extension is now PAUSED. All patterns will not be blocked.");
             } else {
                 // extension is currently on pause (is not blocking sites)
                 // user wants to UNPAUSE extension
@@ -43,7 +41,7 @@ app.controller('OptionsController', function($scope) {
                 $scope.is_pause = "Pause extension";
                 $scope.button_is_pause_color = "btn-danger";
                 
-                $scope.alert("Extension is now UNPAUSED. All patterns will be blocked.");
+                $scope.alertModal("Extension is now UNPAUSED. All patterns will be blocked.");
             }
 
             chrome.storage.local.get("is_pause", function(data) {
@@ -84,6 +82,13 @@ app.controller('OptionsController', function($scope) {
     };
 
     $scope.save = function(msg) {
+        /*
+        for matching exact URLs, I still need to check for duplicates and be able to remove them inline since I don't want to refresh the page
+        the implementation I did below matching exact URLs doesn't work here becuse it's modifying the existing one
+        since exact URLs aren't modified it'll just check for itself and remove itself which doesn't help
+        i may have to redo the entire save. for now, just know it doesn't remove duplicates for exact URLs
+        you can remove duplicates manually if you managed to add them in and find them
+        */
         var prefix = "*://*.";
         var suffix = "/*";
         var removedEmptyElements = [];
@@ -92,11 +97,6 @@ app.controller('OptionsController', function($scope) {
         for (var i = 0; i < patterns.length; i++) { 
             if (patterns[i].pattern.substring(0, 8) === "https://" || patterns[i].pattern.substring(0, 7) === "http://") {
                 // Matching exact URLs (ex. https://www.youtube.com/watch?v=CDokUdux0rc or https://github.com/trien-hong/ChromeHttpRequestBlocker)
-                // i still need to check for duplicates and be able to remove them inline since I don't want to refresh the page
-                // the implementation I did below this doesn't work here becuse it's modifying the existing one after
-                // since this one isn't modified it'll just check for itself and remove itself which doesn't help
-                // i may have to redo the entire save. for now, just know it doesn't remove duplicates for exact patterns
-                // you can remove duplicates manually if you managed to add them in and find them
                 removedEmptyElements.push(patterns[i].pattern);
             } else if (patterns[i].pattern.substring(0, 6) !== prefix && patterns[i].pattern !== "") {
                 // Matching domains/second-level domains, subdomain(s), file path, and ip adresses (ex. youtube.com, www.google.com, test.com/picture.png, and 12.34.56.78)
@@ -125,9 +125,9 @@ app.controller('OptionsController', function($scope) {
         $scope.backgroundPage.save(removedEmptyElements, function() {
             $scope.$apply(function() {
                 if (msg === undefined) {
-                    $scope.success("Your patterns has been saved. Any pattern(s) that were \"\" (empty) have been removed. If your pattern(s) contains any duplicates, it was also removed (beyond the first).");
+                    $scope.successModal("Your patterns has been saved. Any pattern(s) that were \"\" (empty) have been removed. If your pattern(s) contains any duplicates, it was also removed (beyond the first).");
                 } else {
-                    $scope.success(msg);
+                    $scope.successModal(msg);
                 }
 
                 if ($scope.patterns.length === 0) {
@@ -141,7 +141,7 @@ app.controller('OptionsController', function($scope) {
 
     $scope.exportPatterns = function() {
         if ($scope.patterns.length === 0) {
-            $scope.error("Your patterns seems to be empty. There was nothing to export. Please try adding some websites first.");
+            $scope.errorModal("Your patterns seems to be empty. There was nothing to export. Please try adding some websites first.");
         } else {
             var patterns = $scope.backgroundPage.patterns.map(function(x) {
                 return x;
@@ -159,7 +159,7 @@ app.controller('OptionsController', function($scope) {
                     a.download = fileName;
                     a.click();
                     window.URL.revokeObjectURL(url);
-                    $scope.success("Your patterns has been exported. Please be sure to download it. Also remember that you can always import this file in the future. Depending on the size of your patterns, it may take some time to load.");
+                    $scope.successModal("Your patterns has been exported. Please be sure to download it. Also remember that you can always import this file in the future. Depending on the size of your patterns, it may take some time to load.");
                 };
             }());
         
@@ -178,16 +178,15 @@ app.controller('OptionsController', function($scope) {
 
     $scope.clearPatterns = function() {
         if ($scope.patterns.length === 0) {
-            $scope.error("Your patterns seems to be empty. There was nothing to clear. Try adding some websites first.");
+            $scope.errorModal("Your patterns seems to be empty. There was nothing to clear. Try adding some websites first.");
         } else {
             $scope.function = "clearPatternsConfirm";
-            $scope.confirm("Are you sure you want to clear your current patterns? Depending on the size of your patterns, it may take some time to load.");
+            $scope.confirmModal("Are you sure you want to clear your current patterns? Depending on the size of your patterns, it may take some time to load.");
         }
     };
 
     $scope.clearPatternsConfirm = function(showClearPatternsConfirmModal) {
         var length = $scope.patterns.length;
-        $scope.function = "";
 
         for (var i = 0; i < length; i++) {
             $scope.patterns.splice(0, 1);
@@ -200,34 +199,30 @@ app.controller('OptionsController', function($scope) {
 
     $scope.resetTotalBlocked = function() {
         if ($scope.total_blocked === 0) {
-            $scope.error("Your total blocked request is already at 0.");
+            $scope.errorModal("Your total blocked request is already at 0.");
         } else {
             $scope.function = "resetTotalBlockedConfirmed";
-            $scope.confirm("Are you sure you want to reset your total blocked requests to 0?");
+            $scope.confirmModal("Are you sure you want to reset your total blocked requests to 0?");
         }
     };
 
     $scope.resetTotalBlockedConfirmed = function() {
-        $scope.function = "";
-
         chrome.storage.local.set({'total_blocked': 0}, function() {
 
         });
 
         $scope.total_blocked = 0;
-        $scope.success("Your total blocked requests has been reset. It is now 0.");
+        $scope.successModal("Your total blocked requests has been reset. It is now 0.");
 
         chrome.runtime.sendMessage({type: "reload-background-script"});
     };
 
     $scope.uploadFile = function() {
         $scope.function = "uploadFileConfirmed";
-        $scope.confirm("Importing patterns from a file will overwrite your current patterns. Depending on the size of your patterns, it may take some time to load.");
+        $scope.confirmModal("Importing patterns from a file will overwrite your current patterns. Depending on the size of your patterns, it may take some time to load.");
     };
 
     $scope.uploadFileConfirmed = function() {
-        $scope.function = "";
-        
         try {
             var file = document.getElementById('file').files[0];
 
@@ -253,16 +248,16 @@ app.controller('OptionsController', function($scope) {
 
                 reader.readAsBinaryString(file);
             } else {
-                $scope.error("The file you've uploaded doesn't seem to be a text file. Please try a different file or try again.");
+                $scope.errorModal("The file you've uploaded doesn't seem to be a text file. Please try a different file or try again.");
             }
         } catch {
-            $scope.error("You did not choose a file to upload or there seems to be an error with uploading and/or reading your file. Please try a different file or try again.");
+            $scope.errorModal("You did not choose a file to upload or there seems to be an error with uploading and/or reading your file. Please try a different file or try again.");
         }
     };
 
     $scope.scrollUp = function() {
         var objDiv = document.getElementsByClassName("patterns")[0];
-        objDiv.scrollTo({ top: 0, behavior: "smooth" });
+        objDiv.scrollTo({top: 0, behavior: "smooth"});
     };
 
     $scope.scrollDown = function() {
@@ -272,27 +267,27 @@ app.controller('OptionsController', function($scope) {
         objDiv.scrollTo({top: objDiv.scrollHeight, behavior: "smooth"});
     };
 
-    $scope.confirm = function(message, title) {
-        $scope.show_modal_confirm = true;
-        $scope.show_modal_close = false;
+    $scope.confirmModal = function(message, title) {
+        $scope.show_modal_confirm_button = true;
+        $scope.show_modal_close_button = false;
         $scope.modal(message, title || "PLEASE CONFIRM", "text-black");
     };
 
-    $scope.alert = function(message, title) {
-        $scope.show_modal_confirm = false;
-        $scope.show_modal_close = true;
+    $scope.alertModal = function(message, title) {
+        $scope.show_modal_confirm_button = false;
+        $scope.show_modal_close_button = true;
         $scope.modal(message, title || "ALERT", "text-info");
     };
 
-    $scope.success = function(message, title) {
-        $scope.show_modal_confirm = false;
-        $scope.show_modal_close = true;
+    $scope.successModal = function(message, title) {
+        $scope.show_modal_confirm_button = false;
+        $scope.show_modal_close_button = true;
         $scope.modal(message, title || "SUCCESS", "text-success");
     };
 
-    $scope.error = function(message, title) {
-        $scope.show_modal_confirm = false;
-        $scope.show_modal_close = true;
+    $scope.errorModal = function(message, title) {
+        $scope.show_modal_confirm_button = false;
+        $scope.show_modal_close_button = true;
         $scope.modal(message, title || "ERROR", "text-danger");
     };
 
