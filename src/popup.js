1101de95693dbfce4938f98854063ac3ad90c42b
trien-hong <danielhong55@gmail.com>
2023-10-30 06:16:09 -0400

    updated popup.html & popup.js for consistency

--- src/popup.js
+++ src/popup.js
@@ -30,8 +30,6 @@ app.controller('PopupController', function($scope, currentSite) {
 
     $scope.button_is_pause_color = $scope.backgroundPage.button_is_pause_color;
 
-    $scope.is_pause = $scope.backgroundPage.is_pause;
-
     var timeStamp = new Date();
 
     if ($scope.backgroundPage.total_blocked_per_day[timeStamp.getMonth() + 1 + "/" + timeStamp.getDate() + "/" + timeStamp.getFullYear()] === undefined) {
@@ -41,6 +39,42 @@ app.controller('PopupController', function($scope, currentSite) {
     }
 
     $scope.total_blocked = $scope.backgroundPage.total_blocked;
+
+    $scope.is_pause = $scope.backgroundPage.is_pause;
+
+    chrome.storage.local.get('is_pause', function(data) {
+        var is_pause = data.is_pause;
+
+        $scope.pause = function() {
+            if (is_pause === undefined || is_pause === false) {
+                // extension is currently not on pause (is blocking sites)
+                // user wants to PAUSE extension
+                chrome.storage.local.set({'is_pause': true}, function() {
+                
+                });
+                $scope.is_pause = "Unpause Extension";
+                $scope.button_is_pause_color = "btn-success";
+                
+                $scope.alertModal("Extension is now <b><u>PAUSED</u></b>.<br><br>All patterns will not be blocked.");
+            } else {
+                // extension is currently on pause (is not blocking sites)
+                // user wants to UNPAUSE extension
+                chrome.storage.local.set({'is_pause': false}, function() {
+                
+                });
+                $scope.is_pause = "Pause Extension";
+                $scope.button_is_pause_color = "btn-danger";
+
+                $scope.alertModal("Extension is now <b><u>UNPAUSED</u></b>.<br><br>All patterns will be blocked.");
+            }
+
+            chrome.storage.local.get('is_pause', function(data) {
+                is_pause = data.is_pause;
+            });
+
+            chrome.runtime.sendMessage({type: "reload-background-script"});
+        }
+    });
 
     currentSite.getUrl(function(url) {
         $scope.website = url;
@@ -125,49 +159,15 @@ app.controller('PopupController', function($scope, currentSite) {
         });
     };
 
-    chrome.storage.local.get('is_pause', function(data) {
-        var is_pause = data.is_pause;
-
-        $scope.pause = function() {
-            if (is_pause === undefined || is_pause === false) {
-                // extension is currently not on pause (is blocking sites)
-                // user wants to PAUSE extension
-                chrome.storage.local.set({'is_pause': true}, function() {
-                
-                });
-                $scope.is_pause = "Unpause Extension";
-                $scope.button_is_pause_color = "btn-success";
-                
-                $scope.alertModal("Extension is now <u>PAUSED</u>. All patterns will not be blocked.");
-            } else {
-                // extension is currently on pause (is not blocking sites)
-                // user wants to UNPAUSE extension
-                chrome.storage.local.set({'is_pause': false}, function() {
-                
-                });
-                $scope.is_pause = "Pause Extension";
-                $scope.button_is_pause_color = "btn-danger";
-
-                $scope.alertModal("Extension is now <u>UNPAUSED</u>. All patterns will be blocked.");
-            }
-
-            chrome.storage.local.get('is_pause', function(data) {
-                is_pause = data.is_pause;
-            });
-
-            chrome.runtime.sendMessage({type: "reload-background-script"});
-        }
-    });
-
     // I will try to find a better solution for all these different modals later
 
-    $scope.confirmModal = function(message, functionVariable, parameterVariable) {
+    $scope.confirmModal = function(message, confirmFunctionVariable, confirmParameterVariable) {
         $scope.show_modal_confirm_icon = true;
         $scope.show_modal_alert_icon = false;
         $scope.show_modal_success_icon = false;
         $scope.show_modal_error_icon = false;
-        $scope.function = functionVariable;
-        $scope.parameter = parameterVariable;
+        $scope.confirm_function = confirmFunctionVariable;
+        $scope.confirm_parameter = confirmParameterVariable;
         $scope.show_modal_confirm_button = true;
         $scope.show_modal_close_button = false;
         $scope.modal("PLEASE CONFIRM", message, "text-black");
